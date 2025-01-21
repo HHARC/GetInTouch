12b15e89f04cd0de72afb3ee27edc087b32f8de6
HHARC <hharc123@gmail.com>
2025-01-21 11:19:26 -0800

    WTF

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from "react";
 import "./App.css";
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginSignup from "./pages/LoginSingUp";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/Home" element={<Home />} />
+          <Route path="/" element={<LoginSignup />} />
+        </Routes>
+      </Router>
     </>
   );
 }
